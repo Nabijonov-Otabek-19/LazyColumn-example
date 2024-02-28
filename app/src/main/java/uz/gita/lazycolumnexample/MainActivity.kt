643da70b893823d8562b80bf66f74289c41881b4
Otabek Nabijonov <nabijonovo23@gmail.com>
2024-02-28 19:25:12 +0300

    updated some UI parts

--- app/src/main/java/uz/gita/lazycolumnexample/MainActivity.kt
+++ app/src/main/java/uz/gita/lazycolumnexample/MainActivity.kt
@@ -8,7 +8,10 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.*
 import kotlinx.coroutines.flow.distinctUntilChanged
 import uz.gita.lazycolumnexample.ui.theme.LazyColumnExampleTheme
@@ -19,12 +22,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LazyColumnExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting()
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    SyncTwoLazyList()
                 }
             }
         }
@@ -32,23 +31,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting() {
+fun SyncTwoLazyList() {
 
     val firstListState = rememberLazyListState()
     val secondListState = rememberLazyListState()
 
-    val index = remember { mutableStateOf(0) }
+    var currentIndex by rememberSaveable { mutableStateOf(0) }
 
     Column(modifier = Modifier.fillMaxSize()) {
 
         LazyRow(state = firstListState) {
             items(firstList.size) {
-                Card(modifier = Modifier
-                    .padding(8.dp)
-                    .clickable {
-                        index.value = it
-                    }) {
-                    Text(text = firstList[it], modifier = Modifier.padding(8.dp), fontSize = 18.sp)
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .clickable { currentIndex = it },
+                    shape = RoundedCornerShape(16),
+                    colors = CardDefaults.cardColors(
+                        containerColor = if (currentIndex == it) Color.Blue else Color.Red
+                    )
+                ) {
+                    Text(
+                        text = firstList[it],
+                        modifier = Modifier.padding(8.dp),
+                        fontSize = 18.sp,
+                        color = if (currentIndex == it) Color.White else Color.Black
+                    )
                 }
             }
         }
@@ -62,15 +70,20 @@ fun Greeting() {
         ) {
             items(secondList) { item ->
                 Card(modifier = Modifier.padding(8.dp)) {
-                    Text(text = item, modifier = Modifier.padding(8.dp), fontSize = 18.sp)
+                    Text(
+                        text = item,
+                        modifier = Modifier.padding(8.dp),
+                        fontSize = 18.sp
+                    )
                 }
             }
         }
     }
 
-
-    LaunchedEffect(index.value) {
-        secondListState.animateScrollToItem(index = index.value)
+    // when firstListItem clicked, secondList will be scrolled
+    // to index which clicked
+    LaunchedEffect(currentIndex) {
+        secondListState.animateScrollToItem(index = currentIndex)
     }
 
 // Sync the scrolling of the two lists
@@ -78,6 +91,7 @@ fun Greeting() {
         snapshotFlow { secondListState.firstVisibleItemIndex }
             .distinctUntilChanged()
             .collect { index ->
+                currentIndex = index
                 firstListState.animateScrollToItem(index)
             }
     }
